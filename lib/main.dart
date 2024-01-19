1e306b6031e3e02cf850653657d1c249283c0c43
Matt <matt.garvin.416@gmail.com>
2024-01-18 22:33:51 -0500

    added animated clouds

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,80 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+
+  //  set up bottom cloud set ( "cloudset_1" )
+  late AnimationController cloudset_1_controller;     //  it needs an animation controller  
+  final double  cloudset_1_left_max = 1000;           //  the maximum size of the loop (which is half of the width of the cloud image)
+  double        cloudset_1_left_current = 1000;       //  the current position of the loop
+  final int cloudset_1_loop_duration  = 300000;       //  how long in ms is animaion loop?    
+
+  //  set up top cloud set ( "cloudset_2" )
+  late AnimationController cloudset_2_controller;     
+  final double  cloudset_2_left_max = 1000;           
+  double        cloudset_2_left_current = 1000;       
+  final int cloudset_2_loop_duration  = 100000;       //  much slower moving   
+
+    // (this page) init and dispose
+  @override
+  void initState() {
+    super.initState();
+    //  COMMENT
+
+    //  START: BOTTOM CLOUDS
+    cloudset_1_controller = AnimationController(
+      duration: Duration( milliseconds: cloudset_1_loop_duration ), // 
+      vsync: this, 
+    );
+    cloudset_1_controller.forward();     //  this sets the clouds into motion
+
+    cloudset_1_controller.addListener(() {
+      //  it needs a listener to update the cloud position based on
+      //  the ticker value
+      setState(() {
+        cloudset_1_left_current = 0 - (cloudset_1_left_max * cloudset_1_controller.value);
+      });
+      //  Utils.log( filename, cloudset_2_controller.value.toString() );
+    });
+
+    cloudset_1_controller.addStatusListener((status) {
+      //  it needs a status listener to repeat after complete  
+      if (status == AnimationStatus.completed) { cloudset_1_controller.repeat(); }
+    });    
+    //  END:  BOTTOM CLOUDS    
+
+
+    //  START: TOP CLOUDS
+    cloudset_2_controller = AnimationController(
+      duration: Duration( milliseconds: cloudset_2_loop_duration ), // 
+      vsync: this, 
+    );
+    cloudset_2_controller.forward();     //  this sets the clouds into motion
+
+    cloudset_2_controller.addListener(() {
+      //  it needs a listener to update the cloud position based on
+      //  the ticker value
+      setState(() {
+        cloudset_2_left_current = 0 - (cloudset_2_left_max * cloudset_2_controller.value);
+      });
+      //  Utils.log( filename, cloudset_2_controller.value.toString() );
+    });
+
+    cloudset_2_controller.addStatusListener((status) {
+      //  it needs a status listener to repeat after complete  
+      if (status == AnimationStatus.completed) { cloudset_2_controller.repeat(); }
+    });    
+    //  END: TOP CLOUDS
+
+  }
+
+  @override
+  void dispose() {
+    //  dispode of the cloudset controllers on exit
+    cloudset_1_controller.dispose();
+    cloudset_2_controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Container(
         //  set underlying Container to take up the entire screen
         //  ( only width needed for this, cuz the child Column will push height... ) 
-          color: Colors.green,
+          color: Colors.transparent,
           width: double.infinity,
           child: Stack(
             children: [
@@ -61,6 +134,27 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),   
                 ),              
 
+
+
+
+
+                // ***********************************************
+                // cloudset_1 ( the grey, slow, bottom cloud set )
+                // ***********************************************
+                Positioned(
+                  left: cloudset_1_left_current,
+                  top: 0,
+                  child: Image.asset('./assets/images/cloudset_bottom.png'),
+                ),
+
+                // *****************************************
+                // cloudset_2 ( the whiter, faster clouds )
+                // *****************************************          
+                Positioned(
+                  left: cloudset_2_left_current,
+                  top: 0,
+                  child: Image.asset('./assets/images/cloudset_top.png'),
+                ),
 
 
                 // ****************************
@@ -151,7 +245,9 @@ class _MyHomePageState extends State<MyHomePage> {
                       //alignment: Alignment.center,
                     ),
                   ),   
-                ),                                 
+                ),                       
+
+
 
             ]
           ),
